ac6faffae4579f1cd0602db9b3949204ff8f83eb
NAMRATA <59927782+Namrata-Agrawal@users.noreply.github.com>
2021-11-27 01:34:29 +0530

    Add Intent classification

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import flask
 import json
 import os
 import os
-# import dialogflow
+
 import requests
 import json
 # import pusher
-from flask import Flask, request, jsonify, render_template
+from flask import request
 
 app = flask.Flask(__name__)
 
@@ -19,14 +19,29 @@ def home():
 @app.route('/', methods=['POST'])
 def webhook():
     req = request.get_json(force=True)
+    print("hello world")
     print(req)
+    try:
+        if req:
+            print('The response Id ,{}'.format(req['responseId']))
+            print('Text received from User: ,{}'.format(req['queryResult']['queryText']))
+            intent_name = req['queryResult']['intent']['displayName']
+            print('The intent discovered, {}'.format(req['queryResult']['intent']['displayName']))
+        if intent_name == "Enzo_navigate":
+            return {
+                'fulfillmentText': 'Do you want to make sure?'
+            }
+        else:
+            return {
+                'fulfillmentText': 'I need more training to reply you better'
+            }
 
-    return {
-        'fulfillmentText': 'Hello from the bot world'
-    }
+    except:
+        print("Error")
+        return {
+            'fulfillmentText': 'Sorry , I could not understand. Please try again'
+        }
 
 
 if __name__ == "__main__":
-    # app.secret_key = 'ItIsASecret'
-    app.debug = True
     app.run(debug=True, port=5000)
